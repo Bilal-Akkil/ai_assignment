b0f99fcd15bfb3beac85c7274c5f384cdccc22cc
Bilal Akkil <bilal0akkil@gmail.com>
2022-05-12 23:42:06 +0400

    Add prolog

---
+++ prolog_familyTree/family.pl
@@ -0,0 +1,32 @@
+male(john).
+male(david).
+male(jack).
+male(peter).
+male(tony).
+
+female(mary).
+female(liza).
+female(susan).
+female(ray).
+female(karen).
+
+father(john, peter).
+father(john, mary).
+father(david, liza).
+father(david, tony).
+father(jack, susan).
+father(jack, ray).
+
+mother(susan, peter).
+mother(susan, mary).
+mother(ray, liza).
+mother(ray, tony).
+mother(karen, susan).
+mother(karen, ray).
+
+parent(X,Y):- father(X,A);mother(X,A).
+
+sibling(X,Y):- father(A,X),father(A,Y);mother(A,X),mother(A,Y).
+
+grandfather(X,Y):- father(X,A), father(A,Y);father(X,A),mother(A,Y).
+grandmother(X,Y):- mother(X,A), mother(A,Y);mother(X,A),father(A,Y).
